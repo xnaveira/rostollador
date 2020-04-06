15c05f26fd0ffe219725fcee1632aaeb40fe935d
Xavier Naveira <xnaveira@gmail.com>
2020-04-06 11:34:49 +0200

    WIP

--- rostollador.py
+++ rostollador.py
@@ -1,115 +1,117 @@
 import logging
-from uuid import uuid4
+from datetime import datetime
+from random import randint, choice
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, ParseMode, InputTextMessageContent
-from telnetlib import Telnet
-from threading import Thread
 import os
-import re
 
-from telegram.utils.helpers import escape_markdown
+from youtube_api import YouTubeDataAPI
 
 admin = os.getenv('ADMIN_USER')
 admin_filter = Filters.chat(username=admin)
+youtube_api_key = os.getenv('YOUTUBE_API_KEY')
+yt = YouTubeDataAPI(youtube_api_key)
+nyofla_channel=os.getenv('NYOFLA_YT_CHANNEL')
+last_checked = datetime.fromtimestamp(0)
+channel_rostollador_i_jo = os.getenv('ROSTOLLADOR_I_JO_CHANNEL')
+channel_rostolladors_hab = os.getenv('ROSTOLLADORS_CHANNEL')
 
 updater = Updater(token=os.getenv('TELEGRAM_TOKEN'), use_context=True)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
-# thread_dict = {}
-#
-#
-# class GameThread(Thread):
-#
-#     def __init__(self,telnet_session,context,update):
-#         Thread.__init__(self)
-#         self.telnet_session = telnet_session
-#         self.context = context
-#         self.update = update
-#         self.li = [b'\?\s*$', b'\>\s*$', b'\.\s*$', b'RETURN\:\s*$', b'\:\s*$']
-#         keyboard = [
-#             [InlineKeyboardButton('Norte', callback_data='Norte')],
-#             [InlineKeyboardButton('Sur', callback_data='Sur')],
-#             [InlineKeyboardButton('Este', callback_data='Este')],
-#             [InlineKeyboardButton('Oeste', callback_data='Oeste')],
-#         ]
-#         self.reply_markup = InlineKeyboardMarkup(keyboard)
-#
-#
-#     def run(self):
-#         while self.telnet_session.sock is not None:
-#             s = self.telnet_session.expect(self.li)
-#             logging.debug('The server says {}'.format(repr(s)))
-#             if s[1] is not None:
-#                 if s[0] == 3: #Ends with RETURN: then hit return
-#                     self.telnet_session.write('\n'.encode('latin-1'))
-#                 else:
-#                     self.context.bot.send_message(
-#                         chat_id=self.update.effective_chat.id,
-#                         text='```\n' + s[2].decode('latin-1').rstrip() + '\n```',
-#                         parse_mode='MarkDown')
-#                     self.update.message.reply_text('aa', reply_markup=self.reply_markup)
-#
-#     def stop(self):
-#         self.context.bot.send_message(
-#             chat_id=self.update.effective_chat.id,
-#             text='```\ncerrando conexión...\n```',
-#             parse_mode='MarkDown')
-#         self.telnet_session.close()
-#
-#     def get_telnet_session(self):
-#         return self.telnet_session
-
-
-# def stop(update, context):
-#     thread_dict[update.effective_chat.id].stop()
-#     del thread_dict[update.effective_chat.id]
-#
-#
-# stop_handler = CommandHandler('cerrar', stop)
-# dispatcher.add_handler(stop_handler)
-
 messages_sent = 0
 
-def missatges(update, context):
+def version(update, context):
+    version = '2.2.0'
     global messages_sent
+    messages_sent = messages_sent + 1
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text='```\nHe enviat {} missatges.\n```'.format(messages_sent),
+                             text='La meva versió és {}'.format(version),
                              parse_mode='MarkDown')
 
+version_handler = CommandHandler('versio', version, filters=admin_filter)
+dispatcher.add_handler(version_handler)
 
-missatges_handler = CommandHandler('missatges', missatges, filters=admin_filter)
-dispatcher.add_handler(missatges_handler)
+def total_messages(update, context):
+    global messages_sent
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='He enviat {} missatges.'.format(messages_sent),
+                             parse_mode='MarkDown')
+
+total_messages_handler = CommandHandler('missatges', total_messages, filters=admin_filter)
+dispatcher.add_handler(total_messages_handler)
+
+def say(update, context):
+    global messages_sent
+    messages_sent = messages_sent + 1
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='{}'.format(' '.join(context.args)),
+                             parse_mode='MarkDown')
+
+say_handler = CommandHandler('digues', say, filters=admin_filter)
+dispatcher.add_handler(say_handler)
+
+def latest_nyofla_youtube(unused):
+    global messages_sent
+    global last_checked
+    messages_sent = messages_sent + 1
+    ylist = yt.search(channel_id=nyofla_channel, search_type="video", max_results=1, order_by="date")
+    if len(ylist) > 0:
+        ylatest = ylist[0]
+        if ylatest['video_publish_date'] > last_checked:
+            last_checked = ylatest['video_publish_date']
+            updater.bot.send_message(chat_id=channel_rostollador_i_jo,
+                                     text='{} presenta: {}\n{}'.format(ylatest['channel_title'], ylatest['video_title'], ylatest['video_thumbnail']),
+                                     parse_mode='Html')
 
 
-def hola(update, context):
-    query = update.inline_query.query
-# expr = re.compile('.*[h|H]ola')
-# if expr.search(query) is not None:
-    results = [
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="Caps",
-            input_message_content=InputTextMessageContent(
-                query.upper())),
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="Bold",
-            input_message_content=InputTextMessageContent(
-                "*{}*".format(escape_markdown(query)),
-                parse_mode=ParseMode.MARKDOWN)),
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="Italic",
-            input_message_content=InputTextMessageContent(
-                "_{}_".format(escape_markdown(query)),
-                parse_mode=ParseMode.MARKDOWN))]
+# latest_nyofla_handler = CommandHandler('ultim', latest_nyofla_youtube, filters=admin_filter)
+# dispatcher.add_handler(latest_nyofla_handler)
 
-    update.inline_query.answer(results)
 
-hola_handler = InlineQueryHandler(hola)
-dispatcher.add_handler(hola_handler)
+
+
+def hello(update, context):
+    global messages_sent
+    greetings = ['Hola {}', 'Què tal {}?', 'Com vas {}?', 'Què dius {}?', 'Com ho portes {}?']
+    if percent(60):
+        messages_sent = messages_sent + 1
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=choice(greetings).format(update.effective_user.first_name),
+            parse_mode='MarkDown')
+
+
+hello_handler = MessageHandler(Filters.regex('.*[H|h]ola.*'), hello)
+dispatcher.add_handler(hello_handler)
+
+def good_morning(update, context):
+    global messages_sent
+    greetings = ['Bon dia {}', 'Bon dia tinguis {}']
+    if percent(40):
+        messages_sent = messages_sent + 1
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=choice(greetings).format(update.effective_user.first_name),
+            parse_mode='MarkDown')
+
+
+good_morning_handler = MessageHandler(Filters.regex('.*[B|b]on\s+dia.*'), good_morning)
+dispatcher.add_handler(good_morning_handler)
+
+dispatcher.job_queue.run_repeating(latest_nyofla_youtube, 1800, 0, 'NyoflaYT')
+
+
+#Randomly throws a 100 dice and returns if result less than number
+def percent(number):
+    r = randint(0, 100)
+    if r <= number:
+        return True
+    else:
+        return False
 
 updater.start_polling()
+
